d8bb0b2a5798a6c8084dc3dca55cfe04c22ff202
tianrking <tian.r.king@gmail.com>
2025-01-13 17:40:53 +0800

    Add but Fail

--- lib/MonitorTask/MonitorTask.h
+++ lib/MonitorTask/MonitorTask.h
@@ -1,18 +1,18 @@
-#pragma once
+// #pragma once
 
-#include <Arduino.h>
-#include <SimpleFOC.h>
-#include <MPU6050_tockn.h>
+// #include <Arduino.h>
+// #include <SimpleFOC.h>
+// #include <MPU6050_tockn.h>
 
-class MonitorTask {
-public:
-    static void init(MPU6050* imu, BLDCMotor* m0, BLDCMotor* m1);
-    static void startTask(void* parameter);
+// class MonitorTask {
+// public:
+//     static void init(MPU6050* imu, BLDCMotor* m0, BLDCMotor* m1);
+//     static void startTask(void* parameter);
 
-private:
-    static MPU6050* imu;
-    static BLDCMotor* motor0;
-    static BLDCMotor* motor1;
-    static void task(void* parameter);
-    static void processSerialCommand(const String& cmd);
-};
+// private:
+//     static MPU6050* imu;
+//     static BLDCMotor* motor0;
+//     static BLDCMotor* motor1;
+//     static void task(void* parameter);
+//     static void processSerialCommand(const String& cmd);
+// };
--- lib/MotorTask/MotorTask.h
+++ lib/MotorTask/MotorTask.h
@@ -1,21 +1,21 @@
-#pragma once
+// #pragma once
 
-#include <Arduino.h>
-#include <SimpleFOC.h>
+// #include <Arduino.h>
+// #include <SimpleFOC.h>
 
-class MotorTask {
-public:
-    static void init(BLDCMotor* m0, BLDCMotor* m1);
-    static void startTask(void* parameter);
+// class MotorTask {
+// public:
+//     static void init(BLDCMotor* m0, BLDCMotor* m1);
+//     static void startTask(void* parameter);
     
-private:
-    static BLDCMotor* motor0;
-    static BLDCMotor* motor1;
-    static void task(void* parameter);
-};
+// private:
+//     static BLDCMotor* motor0;
+//     static BLDCMotor* motor1;
+//     static void task(void* parameter);
+// };
 
-// 全局状态变量声明
-extern volatile struct GlobalState {
-    float target0;
-    float target1;
-} state;
+// // 全局状态变量声明
+// extern volatile struct GlobalState {
+//     float target0;
+//     float target1;
+// } state;
